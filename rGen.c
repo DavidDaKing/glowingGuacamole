85393bc7102bc9da02472b37280eb31ec73e96a9
DavidDaKing <106818745+DavidDaKing@users.noreply.github.com>
2025-01-12 17:59:48 -0500

    Reading from green bottles
    then printing each line to the console.

--- rGen.c
+++ rGen.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 extern int randomNumGen(int seed);
 
 extern int createList();
 extern int appendList(int randomNum);
+
+extern void strings2List();
+extern char *getPoetry(int s);
+extern char *getFirstWord(char *line);
 
 struct num_and_str {
     int number;
@@ -14,6 +19,10 @@ struct num_and_str {
 };
 
 struct num_and_str *ns_list;
+
+//adding pointer array
+char *number_map[10] = {"One", "Two", "Three", "Four", "Five", 
+                        "Six", "Seven", "Eight", "Nine", "Ten"};
 
 int main(int argc, char* argv[]){
     int randNum;
@@ -47,6 +56,10 @@ int main(int argc, char* argv[]){
     }
     //new func for link list creation
     createList();
+    strings2List();
+
+    char *line = NULL;
+    printf("Line: %s \n", line);
 
     return 0;
 }
@@ -125,4 +138,71 @@ int appendList(int randNum){
         tempNode->next = newNode;
     }   
     return 0;
+}
+
+void strings2List(){
+    struct num_and_str *tempNode;
+    int i = 0;
+
+    tempNode = ns_list;
+
+    while(tempNode != NULL){
+        tempNode->poetry = getPoetry(i);
+        if(tempNode->poetry != NULL){printf("i = %d, %s\n", i, tempNode->poetry);}
+        i++;
+        tempNode = tempNode->next; 
+    }
+}
+
+char *getPoetry(int num){
+    FILE *fp;
+    char *line[80];
+    char *word, *poetryLine;
+
+    if(num > 9){
+        printf("Wrong arg num: %d\n", num);
+        return NULL;
+    }
+
+    fp = fopen("ten_green_bottles.txt", "r");
+
+    if (fp == NULL){
+        printf("Cannot open file\n");
+        return NULL;
+    }
+    while(fgets(line, 70, fp)){
+        word = getFirstWord(line);
+        if (word != NULL){
+            if (strcmp(word, number_map[num]) == 0){
+                poetryLine = (char *)malloc(strlen(line)+1);
+                if(poetryLine == NULL){
+                    printf("Cannot allocate space for line. %d\n", num);
+                    fclose(fp);
+                    return NULL;
+                }
+                strcpy(poetryLine, line);
+                fclose(fp);
+                return poetryLine;
+            }
+        }
+    }
+    fclose(fp);
+    return NULL;
+}
+
+char *getFirstWord(char *line){
+    static char word[10]; 
+    int i = 0, j = 0;
+
+    while ((line[i] != 0) && isspace(line[i]) ){
+        i++;
+    }
+    if (line[i] == 0){return NULL;}
+    while ((line[i] != 0) && isalpha(line[i])){
+        word[j] = line[i];
+        i++; j++;
+    }
+
+    word[j] = 0;
+    return word;    
 }
